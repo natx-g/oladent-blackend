a6cfefb781195bad3bb21ce03348f91cb0cbd054
Natx García <131837631+natx-g@users.noreply.github.com>
2024-09-09 16:06:58 -0600

    inicio

---
+++ oladent/src/main/java/com/oladent/OladentApplication.java
@@ -0,0 +1,13 @@
+package com.oladent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OladentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OladentApplication.class, args);
+	}
+
+}
---
+++ oladent/src/test/java/com/oladent/OladentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oladent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OladentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
